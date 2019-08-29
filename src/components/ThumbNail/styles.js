00d30071f2c4798ea93009658379a3b41789fca4
David Goulet <gouletdavid95@gmail.com>
2019-08-28 22:22:09 -0400

    Fix car info layout

--- src/components/ThumbNail/styles.js
+++ src/components/ThumbNail/styles.js
@@ -9,6 +9,7 @@ export const Thumb = styled.div`
   padding: 5px;
   background-color: #AAAAAA;
   box-shadow: 10px 10px 10px grey; 
+  margin-bottom: -5px;
   &:hover {
   -webkit-transform: scale(1.2);
         -ms-transform: scale(1.2);
@@ -20,7 +21,6 @@ export const Title = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    
 `;
 
 export const Info = styled.div`
@@ -30,12 +30,12 @@ export const Info = styled.div`
     overflow: hidden;
     text-overflow: ellipsis;
     font-size: 18px;
+    margin-left: 40px;
   }
 @media (max-width: 600px) {
     width: 100%;
-    padding: 20px;
+    padding: 0 40px 0 40px;
 }
-    
 `;
 
 export const Details = styled.div`
