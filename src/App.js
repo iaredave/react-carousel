c4282efe9488d1e68d98a678f94d52784ef7c33e
David Goulet <gouletdavid95@gmail.com>
2019-08-29 22:18:27 -0400

    Npm deploy setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react'
-import {Route} from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Detail from './components/DetailPage/index'
 import Slider from './components/Slider/index'
-import {Header, Body, Footer, ThumbnailContainer} from './components/HomePage/styles'
+import { Header, Body, Footer, ThumbnailContainer } from './components/HomePage/styles'
 
 const App = () => (
-	<React.Fragment>
-		<Header>
-
-		</Header>
-		<Body>
-			<ThumbnailContainer>
-				<Route exact path='/' component={Slider}/>
-			</ThumbnailContainer>
-			<Route path='/detailpage/:id' component={Detail}/>
-		</Body>
-		<Footer>
-
-		</Footer>
-	</React.Fragment>
-);
+  <React.Fragment>
+    <Header></Header>
+    <Body>
+      <ThumbnailContainer>
+        <Route exact path='/' component={Slider} />
+      </ThumbnailContainer>
+      <Route path='/detailpage/:id' component={Detail} />
+    </Body>
+    <Footer></Footer>
+  </React.Fragment>
+)
 
 export default App
--- src/components/HomePage/styles.js
+++ src/components/HomePage/styles.js
@@ -1,30 +1,31 @@
 import styled from 'styled-components'
 
 export const Header = styled.div`
-    height: 20vh;
-    background: #FFFFFF;
-`;
+  height: 20vh;
+  background: #ffffff;
+`
 
 export const Body = styled.div`
-    height: 60vh;
-    background-color: #FFFFFF;
-    overflow: scroll;
-    ::-webkit-scrollbar {display:none;}
-    
-`;
+  height: 60vh;
+  background-color: #ffffff;
+  overflow: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`
 
 export const ThumbnailContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 40px;
-    -moz-box-shadow:inset 1px -1px 34px #111111;
-	-webkit-box-shadow:inset 1px -1px 34px #111111;
-	box-shadow:inset 1px -1px 70px #111111;
-	border-radius: 2px;
-`;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 40px;
+  -moz-box-shadow: inset 1px -1px 34px #111111;
+  -webkit-box-shadow: inset 1px -1px 34px #111111;
+  box-shadow: inset 1px -1px 70px #111111;
+  border-radius: 2px;
+`
 
 export const Footer = styled.div`
-    height: 20vh;
-    background: #FFFFFF;
-`;
+  height: 20vh;
+  background: #ffffff;
+`
