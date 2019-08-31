75861fd78f74f1688748044bb9f1ff76b07a1609
David Goulet <gouletdavid95@gmail.com>
2019-08-31 18:39:08 -0400

    New dodge viper pic

--- src/carAPI.js
+++ src/carAPI.js
@@ -27,7 +27,7 @@ const carAPI = [
       'cillum dolore eu fugiat nulla pariatur. Excepteur sint' +
       ' occaecat cupidatat non proident, sunt in culpa qui ' +
       'officia deserunt mollit anim id est laborum."',
-    url: './images/316_main_l.jpg',
+    url: './images/dodge-viper-rt-10-01.jpg',
   },
   {
     id: 2,
